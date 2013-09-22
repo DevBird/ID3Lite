0774117acdda98404809d806fe8a694dbf10a061
tensimiku <tensimiku@gmail.com>
2013-09-23 00:19:25 +0900

    tagData -> Genretostring

--- Version1.cs
+++ Version1.cs
@@ -239,7 +239,7 @@ namespace ID3Lite
                     
                     if (tag.Genre[0] != 0xff)
                     {
-                        tagData.Genre = tag.Genre[0].ToString();
+                        tagData.Genre = GenretoString(tag.Genre[0]);;
                     }
                 }
 
