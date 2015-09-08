57ac9b037aa15545c23c2a3e6169573e8224108c
mystika <devbirdlee@gmail.com>
2015-09-08 16:27:24 +0900

    fix error

--- ID3Lite.cs
+++ ID3Lite.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -182,13 +182,13 @@ namespace ID3Lite
         }
         
         public void SetFrameText(string FrameName, byte[] FrameData){
-            frames[FrameName] = frameData;
+            frames[FrameName] = FrameData;
         }
         
         public void Save()
         {
             byte[] data, totalLength = new byte[4];
-            long fullLength;
+            long fullLength = 0;
             int i;
 
             for (i = 3; i >= 0; i--)
